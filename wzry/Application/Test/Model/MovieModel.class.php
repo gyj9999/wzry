5dd409a84b35f4210793ab3fd24207dec4365b06
Demo <gyj9999@sina.cn>
2018-01-25 17:49:35 +0800

    初始信息

--- wzry/Application/Test/Model/MovieModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-require FRAMEWORK.'Model.class.php';
-class MovieModel extends Model{
-    function getMovies(){
-        $sql = "select * from movie";
-       return $this->link->getAll($sql);
-    }
-}
-
--- wzry/Framework/Model.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-//所有模型类的父类
-class Model{
-    protected $link;
-    function __construct() {
-       // require FRAMEWORK.'mysqlDB.class.php';
-        $this->link = mysqlDB::getInstance(["dbname"=>"wzry"]);
-    }
-}
